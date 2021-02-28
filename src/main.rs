ad0c299b2f30e107952b818d9fa77d5e05128636
Igor Gueths <igor.gueths@protonmail.com>
2021-02-27 19:23:33 -0600

    Add command argument parsing.

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,12 @@ fn main() -> io::Result<()> {
 		stdout().flush()?;
 		let mut input = String::new();
 		 stdin().read_line(&mut input)?;
-		let command = input.trim();
-		let mut exec = Command::new(command).spawn()?;
+		 // Let's make sure to handle any arguments that
+		 //may come after the command.
+let mut input_parts = input.trim().split_whitespace();
+let command = input_parts.next().unwrap();
+let args = input_parts;		
+let mut exec = Command::new(command).args(args).spawn()?;
 		exec.wait()?;
 	}
 }
